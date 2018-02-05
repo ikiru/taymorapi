b99be0a4fed3c64f7f6bb7062ab574c2b7260c25
jeff@rhinofleettracking.com <jeff.a.winkler@gmail.com>
2018-02-05 17:13:30 -0600

    added basic graphql

--- schemas/schema.js
+++ schemas/schema.js
@@ -10,23 +10,42 @@ const{
 } = graphql;
 
 const owners =[
-  {ownerID: 1,txtBusinessName: 'bill',textContactFName: 'Jeff',textContactLName: 'winkler',textPhone:'785-555-0000' ,textEmail: 'adam@eve.net', textAddress: '734 home', textCity:'Dallas', textState: 'TX' ,intActiveOwner: 0},
-  {ownerID: 2,txtBusinessName: 'joey',textContactFName: 'Jeff',textContactLName: 'winkler',textPhone:'785-555-0000' ,textEmail: 'adam@eve.net', textAddress: '734 home', textCity:'Dallas', textState: 'TX' ,intActiveOwner: 1}
+  {ownerID: 1,txtBusinessName: "bill",txtContactFName: "Jeff",txtContactLName: "winkler",txtPhone:"785-555-0000" ,txtEmail: 'adam@eve.net', txtAddress: "734 home", txtCity:'Dallas', txtState: "TX" ,activeLevelID: 0},
+  {ownerID: 2,txtBusinessName: "joey",txtContactFName: "Jeff",txtContactLName: "winkler",txtPhone:"785-555-0000" ,txtEmail: 'adam@eve.net', txtAddress: "734 home", txtCity:'Dallas', txtState: "TX" ,activeLevelID: 1}
 ];
+
+const activeLevel = [
+  {activeLevelID:1, txtActiveLevelName: "Active"},
+  {activeLevelID:2, txtActiveLevelName: "InactiveActive"},
+  {activeLevelID:3, txtActiveLevelName: "Discontinued"}
+]
+
+const ActiveLevelType = new GraphQLObjectType({
+  name: 'ActiveLevel',
+  fields: {
+    activeLevelID: { type: GraphQLInt },
+    txtActiveLevelName: {type: GraphQLString }
+  }
+})
 
 const OwnerType = new GraphQLObjectType({
   name:'Owner',
   fields:{
     ownerID:{type: GraphQLInt} ,
     txtBusinessName:{type: GraphQLString} ,
-    textContactFName: {type: GraphQLString},
-    textContactLName: {type: GraphQLString},
-    textPhone: {type: GraphQLString},
-    textEmail: {type: GraphQLString},
-    textAddress: {type: GraphQLString},
-    textCity: {type: GraphQLString},
-    textState: {type: GraphQLString},
-    intActiveOwner: {type: GraphQLInt}
+    txtContactFName: {type: GraphQLString},
+    txtContactLName: {type: GraphQLString},
+    txtPhone: {type: GraphQLString},
+    txtEmail: {type: GraphQLString},
+    txtAddress: {type: GraphQLString},
+    txtCity: {type: GraphQLString},
+    txtState: {type: GraphQLString},
+    activeLevel: {
+      type: ActiveLevelType,
+      resolve(parentValue, args){
+        return (activeLevel, {activeLevelID: parentValue.activeLevelID})
+      }
+    }
   }
 });
 
@@ -37,7 +56,7 @@ const RootQuery = new GraphQLObjectType({
       type: OwnerType,
       args: { ownerID: {type: GraphQLInt} },
       resolve(parentValue, args) {
-        return _.find(owners , {ownerID: args.ownerID})
+        return _.find(owners, {ownerID: args.ownerID})
       }
     }
   }
--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use('/graphql', expressGraphQL({
   graphiql: true
 }))
 
-
 app.listen(4000, ()  => {
+  console.log('--------------------------------------------------')
   console.log('When you sleep, I can hear you when you scream!!!!');
-  
+  console.log('On port 4000');
+  console.log('--------------------------------------------------')
 });
-
